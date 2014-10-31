708f5fe56447bf3cfde8c47b7976f68a5d9c7ed4
Chris G <crwgregory@gmail.com>
2014-10-31 16:49:35 -0600

    homework weekend 10-30

---
+++ WeekendHomeween10-30/WeekendHomeween10-30/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeekendHomeween10_30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                //FizzBuzz(i);
+            }
+
+            for (int i = 92; i > 79; i--)
+            {
+                //FizzBuzz(i);
+            }
+
+            //Yodaizer("People tell me to slow my roll. I'm skreaming out fk that!");
+            //Yodaizer3000("you are good");
+
+
+
+            Console.ReadKey();
+        }
+
+
+
+        /// <summary>
+        /// If the sentence has 3 words it will become words to live by.
+        /// </summary>
+        /// <param name="text">sentence</param>
+        static void Yodaizer3000(string text)
+        {
+            string[] stringArray = text.Split(' ');
+
+            if (stringArray.Length == 3)
+            {
+                Console.WriteLine(stringArray[2] + ", " + stringArray[0] + " " + stringArray[1]);
+            }
+        }
+
+        /// <summary>
+        /// The orginal trademark yodaizer
+        /// </summary>
+        /// <param name="text">say somethin</param>
+        static void Yodaizer(string text)
+        {
+            string[] stringArray = text.Split(' ');
+            for (int i = stringArray.Length - 1; i >= 0; i--)
+            {
+                Console.WriteLine(stringArray[i]);
+            }
+        }
+
+
+        /// <summary>
+        /// The infamus fizzbuzz program
+        /// </summary>
+        /// <param name="num1">first number entered by user</param>
+        /// <param name="num2">second number entered by user</param>
+        static void FizzBuzz(int number)
+        {
+            
+                if (number == 0)
+                {
+                    Console.WriteLine(number); 
+                }
+                else if (number % 3 == 0 && number % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (number % 3 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else if (number % 5 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else
+                {
+                    Console.WriteLine(number);
+                }
+            
+        }
+    }
+}
